efa1c729f1be46abfde569b0b23d14e220871712
HuangRui-Hust <923637318@qq.com>
2021-06-27 11:51:27 +0800

    创建main.cpp文件，写了初始的代码

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void sort(int arr[],int size)
+{
+    
+}
+
+int main()
+{
+    int arr[]={12,4,89,43,21,78};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    sort(arr,size);
+    return 0;
+}
